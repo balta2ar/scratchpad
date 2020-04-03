dc9232969eba308812d45b1b5818412512a06e94
Yuri Bochkarev <baltazar.bz@gmail.com>
2020-04-04 00:41:49 +0200

    leetcode 30 day challenge: solve day 3 maximum subarray, using divide and conquer (ugly)

--- leetcode-30day-challenge/03_maximum-subarray/03_maximum-subarray.py
+++ leetcode-30day-challenge/03_maximum-subarray/03_maximum-subarray.py
@@ -18,20 +18,61 @@ class Solution:
         return best_sum
 
 
+class SolutionDivideAndConquer:
+    def maxSubArray(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        base = 0
+        def helper(left, right):
+            if right - left <= 2:
+                m = max(nums[left:right])
+                l = max(base, nums[left])
+                f = sum(nums[left:right])
+                c = max(nums[left:right])
+                r = max(base, nums[right-1])
+                return m, l, f, c, r
+
+            mid = left + int((right - left) / 2)
+            amax, al, af, ac, ar = helper(left, mid)
+            bmax, bl, bf, bc, br = helper(mid, right)
+            m, l, f, c, r = (
+                max(amax, bmax),               # max
+                max(al, af, af + bl, af + bf), # left
+                af + bf,                       # full
+                max(ac, ar + bl, bc),          # center
+                max(br, bf, ar + bf, af + bf), # right
+            )
+            return m, l, f, c, r
+
+        m, l, f, c, r = helper(0, len(nums))
+        result = max(l, f, c, r)
+        # if all numbers were negative, set result to the max negative number
+        if m < 0 and m < result:
+            result = m
+        return result
+
+
 def assert_eq(actual, expected):
     if actual != expected:
         raise AssertionError('expected: %s, actual: %s' % (expected, actual))
 
 
 def test(input_, output):
-    assert_eq(Solution().maxSubArray(input_), output)
+    #assert_eq(Solution().maxSubArray(input_), output)
+    assert_eq(SolutionDivideAndConquer().maxSubArray(input_), output)
 
 
 if __name__ == '__main__':
+    test([-2, -2, -3, 0, -3, 1, -3], 1)
+    test([1, 2, 3, -10, 1, 2, 4], 7)
+
     test([-2, 1, -3, 4, -1, 2, 1, -5, 4], 6)
     test([1, 2, 3], 6)
     test([1], 1)
     test([-3], -3)
+    test([-1, 0, -3], 0)
+    test([-1, 1, -3], 1)
     test([-1, -3], -1)
     test([1, 2, 3, -10, 1, 2, 4], 7)
     test([1, 2, 4, -10, 1, 2, 3], 7)
